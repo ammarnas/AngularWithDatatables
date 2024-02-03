dfe2a69e17d784597c27c61d3d62e783aea79457
ammarnas <ammarnasser.contact@gmail.com>
2024-02-03 10:32:38 +0300

    add datatables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularWithDatatables';
+  dtOptions: DataTables.Settings = {};
+  
+
+  members = [
+    { name: "John", age: 25, email: "john@example.com" },
+    { name: "Jane", age: 30, email: "jane@example.com" },
+    { name: "Sam", age: 35, email: "sam@example.com" },
+    { name: "Sam", age: 35, email: "sam@example.com" },
+    { name: "Sam", age: 35, email: "sam@example.com" },
+    { name: "Sam", age: 35, email: "sam@example.com" },
+    { name: "Sam", age: 35, email: "sam@example.com" },
+    { name: "Sam", age: 35, email: "sam@example.com" },
+    { name: "Sam", age: 35, email: "sam@example.com" },
+    { name: "Sam", age: 35, email: "sam@example.com" },
+    { name: "Sam", age: 35, email: "sam@example.com" },
+    // Add more members here...
+  ];
+  ngOnInit(): void {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 7
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { DataTablesModule } from 'angular-datatables'
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DataTablesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
